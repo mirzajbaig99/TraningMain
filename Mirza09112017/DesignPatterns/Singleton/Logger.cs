f1d5a6f26f68d24d034c247ce0c1e78643dba30f
U1DCER <U1DCER@GP133260>
2017-09-25 10:55:54 -0400

    singleton

---
+++ Mirza09112017/DesignPatterns/Singleton/Logger.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mirza09112017.DesignPatterns.Singleton
+{
+    public class LoggerWithOutGetter
+    {
+        // making it static make its accessable to the static accessor and also makes it singleton
+        // private static _logger object the singleton instance
+        private static LoggerWithOutGetter _logger;
+
+        // Make the constructor private so it can only instantiated once
+        // and it instance can be retunred by get instance method
+        private LoggerWithOutGetter()
+        {
+
+        }
+
+
+
+        public static LoggerWithOutGetter GetLogger()
+        {
+            if (LoggerWithOutGetter._logger != null)
+            {
+                LoggerWithOutGetter._logger = new LoggerWithOutGetter();
+            }
+
+            return LoggerWithOutGetter._logger;
+        }
+
+    }
+
+    public class Logger
+    {
+        // making it static make its accessable to the static accessor and also makes it singleton
+        // private static _logger object the singleton instance
+        private static Logger _logger;
+
+        // Make the constructor private so it can only instantiated once
+        // and it instance can be retunred by get instance method
+        private Logger()
+        {
+
+        }
+
+        // Simple Getter Setup
+        public static Logger Instance
+        {
+            get{
+                    if (Logger._logger != null)
+                    {
+                        Logger._logger = new Logger();
+                    }
+
+                    return Logger._logger;
+                }
+        }
+
+    }
+}
---
+++ Mirza09112017/DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mirza09112017.DesignPatterns.Singleton
+{
+    public class Singleton
+    {
+        public static void Main(string[] arg)
+        {
+            // Always returns the static singleton instance for the app 
+            var obj1 = Logger.Instance;
+            var obj2 = Logger.Instance;
+
+            Console.WriteLine((obj1 == obj2).ToString());
+            Console.WriteLine(System.Object.ReferenceEquals(obj1, obj2).ToString());
+            Console.ReadKey();
+        }
+    }
+}
